39df1218fef05c67ca6be3dba6e4c696122ba586
jonathjan0397 <jonathan.r.covington@gmail.com>
2026-07-31 22:24:18 +0000

    stage PostgreSQL TLS before server startup

--- deploy/docker/init-postgres-tls.sh
+++ deploy/docker/init-postgres-tls.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -eu
 
-install -m 0600 /run/secrets/postgres_tls_key "$PGDATA/server.key"
-install -m 0644 /run/secrets/postgres_tls_cert "$PGDATA/server.crt"
+install -d -o postgres -g postgres -m 0700 /run/postgres-tls
+install -o postgres -g postgres -m 0600 /run/secrets/postgres_tls_key /run/postgres-tls/server.key
+install -o postgres -g postgres -m 0644 /run/secrets/postgres_tls_cert /run/postgres-tls/server.crt
+
+exec /usr/local/bin/docker-entrypoint.sh "$@"
